43c1f16b0b7d873c7d1010aa2af37943314d284a
sjaturner <sjaturner@googlemail.com>
2020-06-28 10:20:05 +0100

    cnlohr is very clever to have made this https://github.com/cnlohr/rawdrawandroid.git 9265128b50ef8453b0c915fc33cf4a6503ec9d00

---
+++ rawdraw/CNFG.c
@@ -0,0 +1,22 @@
+//Include this file to get all of rawdraw.  You usually will not
+//want to include this in your build, but instead, #include "CNFG.h"
+//after #define CNFG_IMPLEMENTATION in one of your C files.
+
+#if defined(WINDOWS) || defined(WIN32) || defined(WIN64)
+#include "CNFGWinDriver.c"
+#elif defined( EGL_LEAN_AND_MEAN )
+#include "CNFGEGLLeanAndMean.c"
+#elif defined( __android__ ) || defined( ANDROID )
+#include "CNFGEGLDriver.c"
+#elif defined( OSX )
+#include "CNFGCocoaCGDriver.m"
+#else
+#include "CNFGXDriver.c"
+#endif
+
+#include "CNFGFunctions.c"
+
+#ifdef CNFG3D
+#include "CNFG3D.c"
+#endif
+
