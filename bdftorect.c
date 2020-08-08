72e7c0823003f0f34a421c0bbe9cd457fb3b6564
sjaturner <sjaturner@googlemail.com>
2020-08-08 14:35:31 +0100

    makes fonty things

--- bdftorect.c
+++ bdftorect.c
@@ -1,7 +1,11 @@
+#include "font_coord.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <limits.h>
+#include <ctype.h>
 
 enum
 {
@@ -29,9 +33,100 @@ void dump_tile(int encoding)
    printf("\n");
 }
 
-int main()
+void code_tile(int encoding)
 {
-   char buf[0x100] = {};
+   struct font_coord codes[Y_ELEMS * X_ELEMS][2] = { };
+   int encoded = 0;
+   int empty = 1;
+
+   do
+   {
+      int max = -INT_MAX;
+      struct font_coord
+      {
+         int row;
+         int col;
+      }
+      best[2] = { };
+
+      empty = 1;
+
+      for(int col = 0; col < Y_ELEMS; ++col)
+      {
+         for(int row = 0; row < X_ELEMS; ++row)
+         {
+            int pixel = tiles[encoding][col][row] == '#';
+
+            if(pixel)
+            {
+               empty = 0;
+               int extend = 0;
+
+               for(extend = 0; extend + row + 1 < X_ELEMS && tiles[encoding][col][row + extend + 1] == '#'; ++extend)
+               {
+               }
+
+               if(extend > max)
+               {
+                  max = extend;
+                  best[0].row = row;
+                  best[0].col = col;
+                  best[1].row = row + extend;
+                  best[1].col = col;
+               }
+
+               for(extend = 0; extend + col + 1 < Y_ELEMS && tiles[encoding][col + extend + 1][row] == '#'; ++extend)
+               {
+               }
+
+               if(extend > max)
+               {
+                  max = extend;
+                  best[0].row = row;
+                  best[0].col = col;
+                  best[1].row = row;
+                  best[1].col = col + extend;
+               }
+            }
+         }
+      }
+
+      if(max >= 0)
+      {
+         codes[encoded][0].row = best[0].row;
+         codes[encoded][0].col = best[0].col;
+         codes[encoded][1].row = best[1].row;
+         codes[encoded][1].col = best[1].col;
+         ++encoded;
+
+         for(int row = best[0].row; row <= best[1].row; ++row)
+         {
+            for(int col = best[0].col; col <= best[1].col; ++col)
+            {
+               tiles[encoding][col][row] = '@';
+            }
+         }
+      }
+   }
+   while(!empty);
+
+   printf("static struct font_coord _0x%02x_font_coords[] = {\n", encoding);
+   if(isprint(encoding))
+   {
+      printf(" /* \"%c\" */", encoding);
+   }
+   printf("\n");
+   for(int index = 0; index < encoded; ++index)
+   {
+      printf("   {%2d,%2d,}, {%2d,%2d,},\n", codes[index][0].row, codes[index][0].col, codes[index][1].row, codes[index][1].col);
+   }
+   printf("};\n");
+   printf("static struct font_strokes _0x%02x_font_strokes = { .length = %d, .font_coords = _0x%02x_font_coords, };\n", encoding, encoded, encoding);
+}
+
+int main(int argc, char *argv[])
+{
+   char buf[0x100] = { };
    int encoding = 0;
    int y = 0;
    int bits = 0;
@@ -40,7 +135,7 @@ int main()
    {
       char *nl = strpbrk(buf, "\r\n");
 
-      
+
       assert(nl);
 
       *nl = 0;
@@ -50,20 +145,17 @@ int main()
       }
       else if(strstr(buf, "ENCODING"))
       {
-         char junk[0x100] = {};
+         char junk[0x100] = { };
 
          if(2 != sscanf(buf, "%s %d", junk, &encoding))
          {
             assert(0);
          }
-
-         printf("ENCODING %d\n", encoding);
       }
       else if(strstr(buf, "BITMAP"))
       {
          y = 0;
          bits = 1;
-//       printf("\n");
       }
       else if(strstr(buf, "ENDCHAR"))
       {
@@ -78,19 +170,16 @@ int main()
          if(y >= 1 && y <= 13)
          {
             int col = 0;
-//          printf("%02d ", y);
             for(int bit = 8; bit > 0; --bit, ++col)
             {
                int row = 13 - y;
 
-//             printf("%c", pattern & 1 << bit ? '#' : ' ');
                assert(encoding < 0x100);
                assert(row >= 0 && row < Y_ELEMS);
                assert(col >= 0 && col < X_ELEMS);
 
                tiles[encoding][row][col] = pattern & 1 << bit ? '#' : ' ';
             }
-//          printf("\n");
          }
 
          ++y;
@@ -99,10 +188,35 @@ int main()
       memset(buf, 0, sizeof buf);
    }
 
+   printf("#include \"font_coord.h\"\n");
    for(int encoding = 0; encoding < 0x100; ++encoding)
    {
-      dump_tile(encoding);
+      if(isprint(encoding))
+      {
+         code_tile(encoding);
+      }
+//    dump_tile(encoding);
    }
+
+   if(argc > 1)
+   {
+      printf("struct font_strokes *font_strokes_%s[0x100] = {", argv[1]);
+   }
+   else
+   {
+      printf("struct font_strokes *font_strokes[0x100] = {");
+   }
+
+   for(int encoding = 0; encoding < 0x100; ++encoding)
+   {
+      if(isprint(encoding))
+      {
+         printf("   [0x%02x] = &_0x%02x_font_strokes,", encoding, encoding);
+         printf(" /* \"%c\" */", encoding);
+         printf("\n");
+      }
+   }
+   printf("};\n");
 }
 
 /*
@@ -114,4 +228,3 @@ int main()
 #  BITMAP
 #  ENDCHAR
 */
-
